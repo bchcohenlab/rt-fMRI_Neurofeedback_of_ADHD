c4a95d8234c6b47b0edfe3a15b40d701c340d39d
julianawall <88854051+julianawall@users.noreply.github.com>
2023-06-23 12:33:42 -0400

    Changed practice settings - MSIT_neurofeedback.py

--- PsychoPy_Scripts/MSIT_neurofeedback.py
+++ PsychoPy_Scripts/MSIT_neurofeedback.py
@@ -20,18 +20,13 @@ from numpy.random import randint, shuffle
 import os  # handy system and path functions
 import sys
 
-# Note: There are comments throughout this code to indicate sections that can or should be changed. 
-# If there is no comment indicating that code can be changed, leave it alone unless you have a good 
-# reason to edit it. 
-
 ############################
 # Define                   #
 ############################
  # Turn fullscreen off for testing on monitor that == not 1024x768
 FULL_SCREEN=False
 
-# Here are some parameters that determine the behavior of the task
-# You shouldn't need to change anything here.
+# CC here are some parameters that determine the behavior of the task
 STIM_ISI_SECONDS = 1.75
 NSTIM_BLOCK = 24
 NBLOCK_ITER = 4
@@ -46,9 +41,6 @@ INTERFERENCE_BLOCK = 444
 
 # Flag determining whether we wish to use the Lumina (will cause task to fail 
 # if Lumina == not found).
-
-# Note: We don't use Lumina at BCH, so any Lumina code throughout the script can be ignored. 
-# The code for our button box setup is later in the script.
 LUMINA = 0
 
 # Value returned by the Lumina when each of the buttons == pressed
@@ -63,8 +55,7 @@ LUMINA_TRIGGER = 4
 globalClock = core.Clock()  # to track the time since experiment started
 routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
 
-# Can change instructions if necessary.
-# Task instructions:
+# Task instructions (actual):
 task_instructions1 = """\
 Every few seconds, a set of three numbers (1, 2, 3, or 0)"""
 task_instructions2 = """\
@@ -86,8 +77,33 @@ Answer as accurately and quickly as possible."""
 task_instructions10="""\
 Press any button when you are ready."""
 
+# Task instructions (practice):
+pract_instructions1 = """\
+Every few seconds, a set of three numbers (1, 2, 3, or 0)"""
+pract_instructions2 = """\
+will appear in the center of the screen."""
+pract_instructions3 = """\
+One number will always be different from the other two."""
+pract_instructions4 = """\
+Press the button corresponding to the identity,"""
+pract_instructions5 = """\
+not the position, of the differing number."""
+pract_instructions6 = """\
+The values corresponding to the buttons are:"""
+pract_instructions7 = """\
+left arrow = 1, down arrow = 2, right arrow = 3"""
+pract_instructions8 = """\
+Answer as accurately and quickly as possible."""
+pract_instructions9 = """\
+During this practice round, you will be told whether"""
+pract_instructions10 = """\
+or not your answers are correct."""
+pract_instructions11="""\
+Press any button when you are ready."""
+
 # The possible stimuli for the control condition
 all_control_stim=['100','020','003']
+
 
 # The possible stimuli for the interference condition
 all_int_stim=['221','212','331','313','112','211','332','233','131','311',\
@@ -104,8 +120,6 @@ expName = 'MSIT'  # from the Builder filename that created this script
 # gui dialogue to get participant id, session number, and type of first
 # block (useful for counterbalancing)
 
-# Can change if there is any other info that you want to be able to change
-# at the beginning of the task. 
 expInfo = {'Participant ID':'',\
            'Session':'001', \
            'Configuration': ['Task', 'Practice'], \
@@ -127,11 +141,6 @@ if expInfo['Configuration'] == 'Practice':
 elif expInfo['Configuration'] == 'Task':
     logging.log(level=logging.EXP, msg="Configuration: Task")
     print("Configuration: Task")
-
-
-# if practice, reconfigure some aspects
-if expInfo['Configuration'] == 'Practice':
-    FIXATION_BUFFER_SECONDS = 3.0
 
 # if interference first, swap stim
 if expInfo['Starting Block'] == 'Interference':
@@ -178,13 +187,10 @@ if LUMINA == 1:
 ####################################
 #
 # Setup files for saving
-# Can change output directory if you want. 
 if not os.path.isdir('msit_data'):
     # if this fails (e.g. permissions) we will get error
     os.makedirs('msit_data')
 
-# Can change the filename also, but make sure the participant ID, session, 
-# and starting block are included regardless.
 filename = 'data' + os.path.sep + \
     '%s_%s_%s_%s_%s' %(expInfo['Participant ID'],\
                                             expInfo['Session'],\
@@ -209,17 +215,9 @@ thisExp = data.ExperimentHandler(name=expName, version='',
 #from psychopy import visual
 
 #Setup the Window
-# You should change this code based on the specifics of your screen. You probably will only need to change 
-# size, pos, monitor, and potentially screen. Uncommented code should create a window that fits the Dell
-# monitors in room 410 and the LCD monitor at the scanner. The code that is commented out fits the screen 
-# of a 13" MacBook Pro. 
-# Note that you will need to set up a new monitor for the LCD in PsychoPy. Instructions here:
-# https://www.psychopy.org/builder/builderMonitors.html
-win = visual.Window(size=(1920, 1080), 
-    #size=(1440, 900), #MacBook size
-    fullscr=False, screen=1, allowGUI=False, allowStencil=False,
-    color=[0,0,0], monitor='LCD', colorSpace='rgb', pos=[0,180],
-    #pos=[0,0], #MacBook position
+
+win = visual.Window(size=(1920, 1080), fullscr=False, screen=1, allowGUI=False, allowStencil=False,
+    color=[0,0,0], monitor='Dell', colorSpace='rgb', pos=[0,180],
     blendMode='avg', useFBO=True, units='norm',
     )
 
@@ -233,6 +231,86 @@ else:
 #########################################
 # Initialize various display components #
 #########################################
+
+# Initialize components for Routine "practice instruct"
+pract_instructClock = core.Clock()
+pract_instruct_text1 = visual.TextStim(win=win, ori=0, name='pract_instruct_text1',
+    text=pract_instructions1,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, 0.5], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+    
+pract_instruct_text2 = visual.TextStim(win=win, ori=0, name='pract_instruct_text2',
+    text=pract_instructions2,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, 0.42], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+    
+pract_instruct_text3 = visual.TextStim(win=win, ori=0, name='pract_instruct_text3',
+    text=pract_instructions3,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, 0.26], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+
+pract_instruct_text4 = visual.TextStim(win=win, ori=0, name='pract_instruct_text4',
+    text=pract_instructions4,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, 0.08], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+
+pract_instruct_text5 = visual.TextStim(win=win, ori=0, name='pract_instruct_text5',
+    text=pract_instructions5,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, 0.0], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+
+pract_instruct_text6 = visual.TextStim(win=win, ori=0, name='pract_instruct_text6',
+    text=pract_instructions6,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, -0.16], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+
+pract_instruct_text7 = visual.TextStim(win=win, ori=0, name='pract_instruct_text7',
+    text=pract_instructions7,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, -.24], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+
+pract_instruct_text8 = visual.TextStim(win=win, ori=0, name='pract_instruct_text8',
+    text=pract_instructions8,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, -0.40], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+    
+pract_instruct_text9 = visual.TextStim(win=win, ori=0, name='pract_instruct_text9',
+    text=pract_instructions9,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, -0.56], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+    
+pract_instruct_text10 = visual.TextStim(win=win, ori=0, name='pract_instruct_text10',
+    text=pract_instructions10,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, -0.62], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+    
+pract_instruct_text11 = visual.TextStim(win=win, ori=0, name='pract_instruct_text11',
+    text=pract_instructions11,
+    font='Arial',anchorHoriz='center', anchorVert='center',
+    pos=[0, -0.78], height=0.08, wrapWidth=1.5,
+    color='white', colorSpace='rgb', opacity=1,
+    depth=0.0)
+
 # Initialize components for Routine "instruct"
 instructClock = core.Clock()
 instruct_text1 = visual.TextStim(win=win, ori=0, name='instruct_text1',
@@ -363,10 +441,131 @@ thanks = visual.TextStim(win=win, ori=0, name='thanks',
 ########################
 # Start the experiment #
 ########################
+
 # Initialize timers
 globalClock = core.Clock()  # to track the time since experiment started
 routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
+#------ Routine " Practice Instruct" -------
+#---------------------------------
+t = 0
+pract_instructClock.reset()  # clock 
+frameN = -1
 
+#update component parameters for each repeat
+ready = event.BuilderKeyResponse()  # create an object of type KeyResponse
+ready.status = NOT_STARTED
+
+# keep track of which components have finished
+pract_instructComponents = []
+pract_instructComponents.append(pract_instruct_text1)
+pract_instructComponents.append(pract_instruct_text2)
+pract_instructComponents.append(pract_instruct_text3)
+pract_instructComponents.append(pract_instruct_text4)
+pract_instructComponents.append(pract_instruct_text5)
+pract_instructComponents.append(pract_instruct_text6)
+pract_instructComponents.append(pract_instruct_text7)
+pract_instructComponents.append(pract_instruct_text8)
+pract_instructComponents.append(pract_instruct_text9)
+pract_instructComponents.append(pract_instruct_text10)
+pract_instructComponents.append(pract_instruct_text11)
+pract_instructComponents.append(ready)
+
+# set every component to not started
+for thisComponent in pract_instructComponents:
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+
+
+#-------Start Routine "pract instruct"-------
+if expInfo['Configuration'] == 'Practice':
+    continueRoutine = True
+else:
+    continueRoutine = False
+while continueRoutine:
+
+    # get current time
+    t = pract_instructClock.getTime()
+    frameN = frameN + 1  # number of completed frames (so 0 == the first frame)
+
+    # update/draw components on each frame
+    for thisComponent in pract_instructComponents:
+        if hasattr(thisComponent, 'status'):
+            # *instruct_text* updates
+            if t >= 0.0 and thisComponent.status == NOT_STARTED:
+            # keep track of start time/frame for later
+                thisComponent.tStart = t  # underestimates by a little under
+                                          # one frame
+                thisComponent.frameNStart = frameN  # exact frame index
+                if hasattr(thisComponent, "setAutoDraw"):
+                    thisComponent.setAutoDraw(True)
+
+    # --- framewise state machine
+    if t >= 0.0 and ready.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        ready.tStart = t  # underestimates by a little under one frame
+        ready.frameNStart = frameN  # exact frame index
+        ready.status = STARTED
+
+        # start keyboard / LUMINA checking by first 
+        # clearing all previous events
+        #
+        # clear the keyboard events
+        event.clearEvents()
+
+        # clear Lumina events
+        if LUMINA == 1:
+            lumina_dev.clear_response_queue()
+
+    if ready.status == STARTED:
+
+        # returns key presses, we are looking for all keys (keyList=None)
+        theseKeys = event.getKeys(keyList=None)
+        if len(theseKeys) > 0:  # at least one key was pressed
+            # a response ends the routine
+            continueRoutine = False
+
+        # check for a LUMINA_TRIGGER from the Lumina box
+        if LUMINA == 1:
+            lumina_dev.poll_for_response()
+            while lumina_dev.response_queue_size() > 0:
+                response = lumina_dev.get_next_response()
+                if response["pressed"]:
+                    # LUMINA debug statement
+                    logging.log(level=logging.EXP,\
+                        msg="Lumina received: %s, %d"%(response["key"],\
+                        response["key"]))
+                    print("Lumina received: %s, %d"%(response["key"],\
+                        response["key"]))
+                    # if we receive a TRIGGER enable the task
+                    if response["key"] == LUMINA_TRIGGER:
+                        continueRoutine = False
+
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        routineTimer.reset() # if we abort early the non-slip timer needs reset
+        break
+
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in pract_instructComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+
+    # check for quit (the [Esc] key)
+    if event.getKeys(["escape"]):
+        core.quit()
+
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine == over or we'll get a blank screen
+        win.flip()
+    else:  # this Routine was not non-slip safe so reset non-slip timer
+        routineTimer.reset()
+
+#-------Ending Routine "practice instruct"-------
+for thisComponent in pract_instructComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+        
 #------ Routine "Instruct" -------
 #---------------------------------
 t = 0
@@ -398,7 +597,10 @@ for thisComponent in instructComponents:
 
 
 #-------Start Routine "instruct"-------
-continueRoutine = True
+if expInfo['Configuration'] == 'Practice':
+    continueRoutine = False
+else:
+    continueRoutine = True
 while continueRoutine:
 
     # get current time
@@ -500,7 +702,10 @@ for thisComponent in triggerComponents:
         thisComponent.status = NOT_STARTED
 
 #-------Start Routine "trigger"-------
-continueRoutine = True
+if expInfo['Configuration'] == 'Practice':
+    continueRoutine = False
+else:
+    continueRoutine = True
 while continueRoutine:
     # get current time
     t = triggerClock.getTime()
@@ -524,9 +729,6 @@ while continueRoutine:
         key_resp_3.clock.reset()  # now t=0
         event.clearEvents(eventType='keyboard')
     if key_resp_3.status == STARTED:
-        # If any of these buttons are pressed, the task will start. At BCH, the scanner
-        # pulse maps to 't', but other common mappings are included here as well. 
-        # Be careful not to hit any of these buttons on your keyboard during a scan.
         theseKeys = event.getKeys(keyList=['num_add', 't','+','5', 's'])
         
         # check for quit:
@@ -590,7 +792,10 @@ for thisComponent in fixationComponents:
         thisComponent.status = NOT_STARTED
 
 #-------Start Routine "fixation"-------
-continueRoutine = True
+if expInfo['Configuration'] == 'Practice':
+    continueRoutine = False
+else:
+    continueRoutine = True
 while continueRoutine and routineTimer.getTime() > 0:
     # get current time
     t = fixationClock.getTime()
@@ -730,7 +935,7 @@ for thisExp_loop in exp_loop:
         first_text.setText(first_text_str)
         first_resp = event.BuilderKeyResponse()  # create an object of type KeyResponse
         first_resp.status = NOT_STARTED
-        # begin routine
+        # CC begin routine
 
 
         # logging
@@ -799,15 +1004,12 @@ for thisExp_loop in exp_loop:
                             if response["key"] in [0,1,2]:
                                 theseKeys.append(str(response["key"]+1))
                 else:
-                    # These are the keys the button box maps to at BCH.
                     theseKeys = event.getKeys(keyList=['b', 'd', 'a', 'c','left',\
                         'down','right'])
 
                 if len(theseKeys) > 0:  # at least one key was pressed
                     first_resp.keys = theseKeys[-1]  # just the last key pressed
                     first_resp.rt = first_resp.clock.getTime()
-                    # Indicates which key maps to which number during the task (for
-                    # the BCH button box)
                     if first_resp.keys == 'left' or first_resp.keys == 'b':
                         first_resp.keys = '1'
                     elif first_resp.keys == 'down' or first_resp.keys == 'd' or first_resp.keys == 'a':
