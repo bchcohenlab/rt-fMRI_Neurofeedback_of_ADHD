13f607c06a60eb319583f9b9aa165b85076eba82
MeghanW23 <89412811+MeghanW23@users.noreply.github.com>
2023-10-17 14:59:00 -0400

    Update msit_preproc_script_motor_cortex.py

--- adhd_rt/msit_preproc_script_motor_cortex.py
+++ adhd_rt/msit_preproc_script_motor_cortex.py
@@ -6,7 +6,7 @@ from nilearn import image, masking
 from nilearn.glm.first_level import FirstLevelModel
 import tempfile
 import nibabel as nib 
-
+import datetime #for generating timestamps
 from nilearn import image
 from nilearn.glm.first_level import FirstLevelModel
 
@@ -37,8 +37,8 @@ sys.path.append(rootPath)
 subjID = sys.argv[1]
 print(f"\n----Starting MSIT----\n")
 
-# load ACC template mask and subject's functional data for analysis
-acc_mask = currPath+'/template_masks/thr_Precentral_Gyrus_fsl_atlas.nii.gz'
+# load motor template mask and subject's functional data for analysis
+motor_mask = currPath+'/template_masks/thr_Precentral_Gyrus_fsl_atlas.nii.gz'
 subj_data_func = image.load_img(os.path.join(dicomPath, '*.nii'))
 
 # skull strip subject's func data
@@ -55,7 +55,7 @@ fmri_glm = FirstLevelModel(t_r=1.06,
                             hrf_model=None, 
                             drift_model='cosine', 
                             high_pass=0.01,
-                            mask_img=acc_mask)
+                            mask_img=motor_mask)
 
 fmri_glm = fmri_glm.fit(subj_data_func, events)
 
@@ -72,4 +72,20 @@ z_map = fmri_glm.compute_contrast(inter_minus_con, output_type='z_score')
 z_map_bin = image.binarize_img(z_map)
 
 # Save to current subject's folder
-nib.save(z_map_bin, os.path.join(currPath, 'subjects', subjID, 'motorcortex_mask_subj_space.nii.gz'))
+
+# Generate a unique filename based on timestamp and a random value
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+unique_filename = f'motor_mask_{timestamp}.nii.gz'
+
+# Construct the directory path
+output_dir = os.path.join(currPath, 'subjects', subjID)
+
+# Ensure the directory exists or create it if it doesn't
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Save the NIfTI image with the unique filename in the subject's folder
+output_file_path = os.path.join(output_dir, unique_filename)
+nib.save(z_map_bin, output_file_path)
+
+print("Length of conditions['Control']:", len(conditions['Control']))
